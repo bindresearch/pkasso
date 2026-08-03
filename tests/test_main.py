f6a2b652df2c6b7942f2aff0dbc2f2c909bea861
Soeren von Buelow <soeren.buelow@bindresearch.org>
2026-08-03 18:21:23 +0100

    Fix protonation and stereochemistry output

--- tests/test_main.py
+++ tests/test_main.py
@@ -816,3 +816,75 @@ def test_construct_mol(smiles_raw, net_charge):
     state_vec = np.array(state_vec)
     mol_cand = main.construct_mol(mol, indices, state_vec)
     assert Chem.GetFormalCharge(mol_cand) == net_charge
+
+
+@pytest.mark.parametrize(
+    ("smiles_raw", "expected_hydrogens"),
+    [
+        ("CN(C)C", 1),
+        ("CN", 3),
+        ("c1ccncc1", 1),
+    ],
+)
+def test_construct_mol_protonation_adds_hydrogen_without_radical(
+    smiles_raw,
+    expected_hydrogens,
+):
+    mol = Chem.MolFromSmiles(smiles_raw)
+    for atom in mol.GetAtoms():
+        atom.SetAtomMapNum(atom.GetIdx() + 1)
+
+    nitrogen = next(atom for atom in mol.GetAtoms() if atom.GetSymbol() == "N")
+    mol_cand = main.construct_mol(
+        mol,
+        [nitrogen.GetAtomMapNum()],
+        np.array([2], dtype=np.int64),
+    )
+    protonated_nitrogen = next(
+        atom for atom in mol_cand.GetAtoms() if atom.GetSymbol() == "N"
+    )
+
+    assert protonated_nitrogen.GetFormalCharge() == 1
+    assert protonated_nitrogen.GetTotalNumHs() == expected_hydrogens
+    assert protonated_nitrogen.GetNumRadicalElectrons() == 0
+
+
+def test_stereochemistry_relaxation_preserves_compatible_carbon_centres():
+    smiles = (
+        "C=C[C@H]1C[N@@]2CC[C@H]1C[C@@H]2[C@@H](OC(=O)OCC)"
+        "c1ccnc2ccc(OC)cc12"
+    )
+    mol, _ = main.preprocess(smiles, tautomer_search=False)
+    nitrogen = next(
+        atom
+        for atom in mol.GetAtoms()
+        if atom.GetSymbol() == "N" and not atom.GetIsAromatic()
+    )
+    nitrogen_map_idx = nitrogen.GetAtomMapNum()
+    protonated = main.construct_mol(
+        mol,
+        [nitrogen_map_idx],
+        np.array([2], dtype=np.int64),
+    )
+
+    relaxed = main.relax_stereochemistry_for_embedding(
+        protonated,
+        {nitrogen_map_idx},
+    )
+
+    specified_carbons = [
+        atom
+        for atom in relaxed.GetAtoms()
+        if atom.GetAtomicNum() == 6
+        and atom.GetChiralTag() != Chem.ChiralType.CHI_UNSPECIFIED
+    ]
+    relaxed_nitrogen = next(
+        atom for atom in relaxed.GetAtoms() if atom.GetAtomMapNum() == nitrogen_map_idx
+    )
+
+    assert len(specified_carbons) == 4
+    assert relaxed_nitrogen.GetChiralTag() == Chem.ChiralType.CHI_UNSPECIFIED
+    assert relaxed_nitrogen.GetFormalCharge() == 1
+    assert relaxed_nitrogen.GetTotalNumHs() == 1
+    assert relaxed_nitrogen.GetNumRadicalElectrons() == 0
+    assert main._can_embed_with_stereochemistry(relaxed)
